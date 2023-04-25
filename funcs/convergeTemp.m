536a6eb3ffe6a6a80067577109f9454c6bfb2537
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2023-04-25 18:23:28 -0400

    Better Documentation

--- funcs/convergeTemp.m
+++ funcs/convergeTemp.m
@@ -8,6 +8,7 @@ h_hel = getHc(Vel,Dh,T_i,P_i);
 num = (h_gas / h_hel) * T_gas + (wall_thick / k) * h_gas * T_gas + T_i;
 den = (h_gas / h_hel) + 1 + (wall_thick / k) * h_gas;
 
+% Derives other values
 T_hw = num / den;
 q_dot = h_gas * (T_gas - T_hw);
 T_cw = -(q_dot * wall_thick / k) + T_hw;
