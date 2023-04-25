0394df0cae83e292808788d1baeabe39354e144f
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2023-04-25 18:22:20 -0400

    Updated  T_hw algorithm

--- funcs/convergeTemp.m
+++ funcs/convergeTemp.m
@@ -1,52 +1,15 @@
-function [q_dot, T_cw, T_hw] = convergeTemp(T_gas, h_gas, k, wall_thick, T_i,Vel,Dh,P_i)
+function [q_dot, T_cw, T_hw] = convergeTemp(T_gas, h_gas, k, wall_thick,...
+    T_i,Vel,Dh,P_i)
 
-T_hw_guess = T_gas - 100;
-
-
+% Grabs helium convection heat transfer coefficient
 h_hel = getHc(Vel,Dh,T_i,P_i);
 
+% Solves system of equations
+num = (h_gas / h_hel) * T_gas + (wall_thick / k) * h_gas * T_gas + T_i;
+den = (h_gas / h_hel) + 1 + (wall_thick / k) * h_gas;
 
-%disp(h_hel)
-
-
-tolerance = 10;
-step = .01;
-
-trials = 1000000;
-i = 1;
-
-T_hw = T_hw_guess;
-
-while i < trials
-
-    q_dot_hc = h_gas*(T_gas - T_hw);
-
-    T_cw = -(q_dot_hc * wall_thick/ k) + T_hw;
-
-    q_dot_cc = h_hel*(T_cw - T_i);
-
-
-   % disp(q_dot_hc)
-    %disp(q_dot_cc)
-
-
-    if abs(q_dot_hc - q_dot_cc) <= tolerance
-
-        q_dot = q_dot_cc;
-        i = trials;
-
-    elseif q_dot_hc > q_dot_cc
-
-        T_hw = T_hw + step;
-        q_dot = 1;
-
-    else
-
-        T_hw = T_hw - step;
-        q_dot = 1;
-
-    end
-
-    i = i + 1;
+T_hw = num / den;
+q_dot = h_gas * (T_gas - T_hw);
+T_cw = -(q_dot * wall_thick / k) + T_hw;
 
 end
