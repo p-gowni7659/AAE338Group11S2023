3c8282ceff9adeaa1a046fc936405dd3b30e465c
jonahfouts <jonahfouts@gmail.com>
2023-04-25 17:47:19 -0400

    Update coolinggrapher.m

--- funcs/coolinggrapher.m
+++ funcs/coolinggrapher.m
@@ -1,4 +1,4 @@
-function [] = coolinggrapher(M_hel_arr, qdot_arr, T_hw_arr, T_cw_arr, T_hel_arr, P_hel_arr, Aratio, x, A, T_gas, h_g_x, Qdot_x, M_x, rho_x, T_x, V_x)
+function [] = coolinggrapher(M_hel_arr, qdot_arr, T_hw_arr, T_cw_arr, T_hel_arr, P_hel_arr, Aratio, xplot, A, T_gas, h_g_x, Qdot_x, M_x, rho_x, T_x, V_x)
 
 figure()
 plot(M_hel_arr, T_hel_arr)
@@ -35,7 +35,7 @@ ylabel("Helium Static Pressure")
 
 % Plot the cross-section
 figure()
-plot(x, sqrt(A/pi));
+plot(xplot, sqrt(A/pi));
 grid on
 hold on
 plot(x3, x4, 'color', [0, 0.4470, 0.7410])
