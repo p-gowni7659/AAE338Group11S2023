d5b062f8214cf26c9b674ad6a2d7510cd4815188
p-gowni7659 <gowni@purdue.edu>
2023-04-25 14:07:26 -0400

    mom?

--- nozzleprops.m
+++ nozzleprops.m
@@ -1,7 +1,7 @@
-function [] = nozzleprops(Dt, Area_arr, gma, Mach_arr, tubenum)
+function [] = nozzleprops(Dt, Area_arr, gma, M_x, tubenum,T_gas)
 
 %Find Area at a distance x from the start of the converging section
-x = tubenum*Dt;
+x = (tubenum*Dt) - (Dt/2);
 
 if x <= contract_L
     area = Area_arr(converge_num * x / contract_L);
@@ -10,13 +10,9 @@ elseif x <= contract_L + nozzle_L
 else
     disp("Spence Sucks")
 end
+Area_arr = 
 
 
-
-Pr = (4 * gma) / ((9 * gma) - 5);
-r = Pr ^ (0.33);
-
-T_gas = T_cns .* (1 + (r .* ((gma - 1) ./ 2) .* M_x)) ./ (1 + (((gma - 1) ./ 2) .* M_x));
 
 
 end
